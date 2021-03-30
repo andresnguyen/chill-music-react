3ad9d497edbfaa8290b6aa32c80d07943747da45
Trần Quốc Thuận <quocthuan251@gmail.com>
2021-03-30 09:39:55 +0700

    feat: create search page

--- src/common/Header/index.jsx
+++ src/common/Header/index.jsx
@@ -7,27 +7,27 @@ function Header() {
     return (
         <div className="header-wrapper">
             <header className="header">
-                <NavLink className="header__item" activeClassName="header__item--active" to="/">
+                <NavLink className="header__item" activeClassName="header__item--active" exact to="/">
                     <i className="fal fa-home"></i>
                     <span>Trang chủ</span>
                 </NavLink>
 
-                <NavLink className="header__item" to="/search">
+                <NavLink className="header__item" exact to="/search" activeClassName="header__item--active">
                     <i className="fal fa-search"></i>
                     <span>Tìm kiếm</span>
                 </NavLink>
 
-                <NavLink className="header__item" to="/library">
+                <NavLink className="header__item" activeClassName="header__item--active" exact to="/library">
                     <i className="fal fa-stream"></i>
                     <span>Thư viện</span>
                 </NavLink>
 
-                <NavLink className="header__item" to="/history">
+                <NavLink className="header__item" activeClassName="header__item--active" exact to="/history">
                     <i className="fal fa-history"></i>
                     <span>Lịch sử</span>
                 </NavLink>
 
-                <NavLink className="header__item" to="/profile">
+                <NavLink className="header__item" activeClassName="header__item--active" exact to="/profile">
                     <i className="fal fa-user-alt"></i>
                     <span>Cá nhân</span>
                 </NavLink>
